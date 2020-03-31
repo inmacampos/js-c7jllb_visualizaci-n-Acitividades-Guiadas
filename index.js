2e8cad35fe24accedbe273b82fde544b758adf3f
inmacampos <inma.campos.sanchez@gmail.com>
2020-03-31 12:00:42 +0200

    1 file modified

--- index.js
+++ index.js
@@ -59,35 +59,15 @@ var data3 = [
         {"year": 1991, "name":"delta2", "value": 17},
         {"year": 1992, "name":"delta2", "value": 35}
       ]
-var myData = [
-  {"Travel Time": "<5 Minutes",    "Population Percentage":  2},
-  {"Travel Time": "15-24 Minutes", "Population Percentage": 30},
-  {"Travel Time": "35-44 Minutes", "Population Percentage":  7},
-  {"Travel Time": "45-89 Minutes", "Population Percentage": 11},
-  {"Travel Time": "5-14 Minutes",  "Population Percentage": 20},
-  {"Travel Time": "90+ Minutes",   "Population Percentage":  5},
-  {"Travel Time": "25-34 Minutes", "Population Percentage": 25}
-]
-
-new d3plus.BarChart()
+var visualization = d3plus.viz()
+  .container("#viz3")
+  .data(data3)
   .config({
-    data: myData,
-    groupBy: "Travel Time",
-    x: "Travel Time",
-    xConfig: {
-      tickFormat: function(d) {
-        return d.replace("Minutes", "").trim();
-      },
-      title: "Commute Time in Minutes"
-    },
-    y: "Population Percentage",
-    yConfig: {
-      tickFormat: function(d) {
-        return d + "%";
-      },
-      title: "Percentage of Population"
-    },
-    shapeConfig: {label: false},
-    legend: false
+    groupBy:(["year"])
   })
-  .render();
+  .type("Whisker")
+  .id("name")
+  .x("year")
+  .y("value")
+  .axes({"ticks" : false})
+  .draw()
