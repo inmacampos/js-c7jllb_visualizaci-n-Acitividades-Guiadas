6edc0ea22e952951131018e6ed6056cbb9a8bcf5
inmacampos <inma.campos.sanchez@gmail.com>
2020-03-31 11:24:57 +0200

    1 file modified

--- index.js
+++ index.js
@@ -41,3 +41,43 @@ var visualization = d3plus.viz()
   .axes({"ticks" : false})
   .draw()
 
+var data3 = [
+        {"year": 1991, "name":"alpha", "value": 15},
+        {"year": 1992, "name":"alpha", "value": 34},
+        {"year": 1991, "name":"alpha2", "value": 17},
+        {"year": 1992, "name":"alpha2", "value": 65},
+        {"year": 1991, "name":"beta", "value": 10},
+        {"year": 1992, "name":"beta", "value": 10},
+        {"year": 1991, "name":"beta2", "value": 40},
+        {"year": 1992, "name":"beta2", "value": 38},
+        {"year": 1991, "name":"gamma", "value": 5},
+        {"year": 1992, "name":"gamma", "value": 10},
+        {"year": 1991, "name":"gamma2", "value": 20},
+        {"year": 1992, "name":"gamma2", "value": 34},
+        {"year": 1991, "name":"delta", "value": 50},
+        {"year": 1992, "name":"delta", "value": 43},
+        {"year": 1991, "name":"delta2", "value": 17},
+        {"year": 1992, "name":"delta2", "value": 35}
+      ]
+new d3plus.BoxWhisker()
+    .config({
+      data: data3,
+      groupBy: ["year", "value"],
+      shapeConfig: {
+        outlier: function(d) {
+          return d.year === "1991" ? "Circle" : "Rect";
+        },
+        outlierConfig: {
+          Rect: {
+            fill: "green"
+          },
+          Circle: {
+            fill: "red"
+          }
+        }
+      },
+      x: "year",
+      y: "value"
+    })
+    .render();
+}
